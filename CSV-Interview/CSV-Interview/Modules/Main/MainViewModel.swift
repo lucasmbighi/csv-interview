e41d4f015d8b1288de6822dc5c4fcbb61fcc5329
Lucas Bighi <lucas.bighi@apoioprodesp.sp.gov.br>
2025-01-20 17:06:44 -0300

    Finish UI elements

--- CSV-Interview/CSV-Interview/Modules/Main/MainViewModel.swift
+++ CSV-Interview/CSV-Interview/Modules/Main/MainViewModel.swift
@@ -9,26 +9,74 @@ import Foundation
 
 final class MainViewModel: ObservableObject {
     
+    // MARK: Published properties
+    @Published var isShowingFileSelector: Bool
+    
+    @Published private(set) var isLoading: Bool
+    @Published private var csvData: [[String]]
+    @Published private(set) var errorMessage: String?
+    
     // MARK: Private properties
     private let csvReader: CSVReader
     
-    // MARK: Published properties
-    @Published private(set) var errorMessage: String?
-    @Published private(set) var csvData: [[String]] = []
-    
-    init(csvReader: CSVReader = CSVReader()) {
+    init(
+        isShowingFileSelector: Bool = false,
+        isLoading: Bool = false,
+        csvData: [[String]] = [],
+        errorMessage: String? = nil,
+        csvReader: CSVReader = CSVReader()
+    ) {
+        self.isShowingFileSelector = isShowingFileSelector
+        self.isLoading = isLoading
+        self.csvData = csvData
+        self.errorMessage = errorMessage
         self.csvReader = csvReader
     }
     
-    func loadCSV(from url: URL) {
+    func showFileSelector() {
+        isShowingFileSelector = true
+        isLoading = true
+    }
+    
+    func onChooseFile(_ result: Result<URL, Error>) {
+        switch result {
+        case .success(let url):
+            guard url.startAccessingSecurityScopedResource() else { return }
+            loadCSV(from: url)
+        case .failure:
+            errorMessage = genericErrorMessage
+        }
+        isLoading = false
+    }
+    
+    private func loadCSV(from url: URL) {
         do {
             csvData = try csvReader.parseCSV(from: url)
         } catch {
             guard let csvReaderError = error as? CSVReaderError else {
-                errorMessage = "Failed to load CSV"
+                errorMessage = genericErrorMessage
                 return
             }
             errorMessage = csvReaderError.errorDescription
         }
+    }
+    
+    func isEven(_ row: [String]) -> Bool {
+        csvData.firstIndex(of: row)?.isMultiple(of: 2) == true
+    }
+}
+
+// MARK: Get-only properties
+extension MainViewModel {
+    private var genericErrorMessage: String {
+        "Failed to load CSV file"
+    }
+    
+    var getCSVHeaders: [String]? {
+        csvData.first
+    }
+    
+    var getCSVData: [[String]] {
+        csvData.filter { $0 != getCSVHeaders }
     }
 }
